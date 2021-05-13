df83acf208d2e3984437fe9d2548c4eaf8012f6c
Ahmet CAN <ahmetcan423507@gmail.com>
2021-05-13 13:23:24 +0300

    index.js,_app.js

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../scss/global.scss'
+import '../scss/navbar.scss'
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar'
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default HomePage

d3952ea0ab45205308a3dd13101bc4a18ff4c408
Ahmet CAN <ahmetcan423507@gmail.com>
2021-05-16 14:35:16 +0300

    add new components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Navbar from '../components/navbar'
+import Accordion from '../components/accordion'
+import Head from 'next/head'
+import Form from '../components/form'
+
 function HomePage() {
   return (
     <div>
+      <Head>
+        <title>Booking.com</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
+      <Accordion />
+      <Form />
     </div>
   )
 }

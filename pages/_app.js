6ad8e4391b199b39d4584e62e923327d92b8707e
Ahmet CAN <ahmetcan423507@gmail.com>
2021-05-16 14:32:26 +0300

    scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../scss/global.scss'
 import '../scss/navbar.scss'
+import '../scss/accordion.scss'
+import '../scss/formSection.scss'
+import '../scss/popup.scss'
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }

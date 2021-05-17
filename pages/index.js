abd69a4183776ee1ef8adb0cae38b9c226f1f291
Ahmet CAN <ahmetcan423507@gmail.com>
2021-05-17 21:40:17 +0300

    add SimpleSlider component and remove head

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import Navbar from '../components/navbar'
 import Accordion from '../components/accordion'
-import Head from 'next/head'
 import Form from '../components/form'
+import SimpleSlider from '../components/SimpleSlider'
 
 function HomePage() {
   return (
     <div>
-      <Head>
-        <title>Booking.com</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <Navbar />
       <Accordion />
       <Form />
+      <SimpleSlider />
     </div>
   )
 }

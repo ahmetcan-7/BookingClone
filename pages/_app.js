4752eca49da13736112fbef6161118e963bf99b5
Ahmet CAN <ahmetcan423507@gmail.com>
2021-05-17 21:38:48 +0300

    add head and remove index.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,15 @@ import '../scss/accordion.scss'
 import '../scss/formSection.scss'
 import '../scss/popup.scss'
 
+import Head from 'next/head'
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>booking.com</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }

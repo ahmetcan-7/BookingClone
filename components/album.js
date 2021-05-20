aed2bb1107296ec4f631a9fb4c3645438b3f6f76
Ahmet CAN <ahmetcan423507@gmail.com>
2021-05-20 21:38:37 +0300

    build album.js component

---
+++ components/album.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+function Album() {
+  return (
+    <div className="album container">
+      <h2 className="album-title">Get inspiration for your next trip </h2>
+
+      <div className="album-images">
+        <a href="#" className="image-link img1">
+          <div
+            className="album-image "
+            style={{
+              backgroundImage: `url("https://cf.bstatic.com/xdata/images/xphoto/540x405/116099712.webp?k=b7fa9c665459dba1a8bf2a2d67b75feba8e9ec94436f2a140c1e0ef358c88f20&o=")`
+            }}
+          ></div>
+          <div className="album-image-article">
+            <h3>A local guide to traveling in Portugal</h3>
+            <p>
+              Follow in the footsteps of local travelers and discover a new side
+              of Portugal.
+            </p>
+          </div>
+        </a>
+        <a href="#" className="image-link img2">
+          <div
+            className="album-image "
+            style={{
+              backgroundImage: `url("https://cf.bstatic.com/xdata/images/xphoto/540x405/116425937.webp?k=610ce7e7cf8db4f46ad380912470a773328d6230500155749e9cc2b3e3343c90&o=")`
+            }}
+          ></div>
+          <div className="album-image-article">
+            <h3>A local guide to traveling in Portugal</h3>
+            <p>
+              Follow in the footsteps of local travelers and discover a new side
+              of Portugal.
+            </p>
+          </div>
+        </a>
+        <a href="#" className="image-link img3">
+          <div
+            className="album-image "
+            style={{
+              backgroundImage: `url("https://cf.bstatic.com/xdata/images/xphoto/540x405/23277800.webp?k=937be7b50e2d6082f5e4da748463e7243474c68dffb82142613c0838f40f7b3e&o=")`
+            }}
+          ></div>
+          <div className="album-image-article">
+            <h3>A local guide to traveling in Portugal</h3>
+            <p>
+              Follow in the footsteps of local travelers and discover a new side
+              of Portugal.
+            </p>
+          </div>
+        </a>
+        <a href="#" className="image-link img4">
+          <div
+            className="album-image "
+            style={{
+              backgroundImage: `url("https://cf.bstatic.com/xdata/images/xphoto/720x405/111367150.webp?k=9c796f631dce3f940ee746afcbd58d12d7d30077349e84bdb73f89fe7de20819&o=")`
+            }}
+          ></div>
+          <div className="album-image-article">
+            <h3>A local guide to traveling in Portugal</h3>
+            <p>
+              Follow in the footsteps of local travelers and discover a new side
+              of Portugal.
+            </p>
+          </div>
+        </a>
+        <a href="#" className="image-link img5">
+          <div
+            className="album-image "
+            style={{
+              backgroundImage: `url("https://cf.bstatic.com/xdata/images/xphoto/540x405/110465249.webp?k=4429b31459fed3b7551f5a4d4912d8afe1dc4e2e388cd34a3364c906db94afac&o=")`
+            }}
+          ></div>
+          <div className="album-image-article">
+            <h3>A local guide to traveling in Portugal</h3>
+            <p>
+              Follow in the footsteps of local travelers and discover a new side
+              of Portugal.
+            </p>
+          </div>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Album
